0cf901c8c16ae659543d19af770d8bb68cfb3a7f
zerowsl <zerols@126.com>
2026-09-02 19:03:01 +0800

    add codes

---
+++ src/Server-Over/Caches/Impls/VsrbStageDataCache.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace ServerOver.Caches;
+
+public class VsrbStageDataCache(IMemoryCache memoryCache) : IVsrbStageDataCache
+{
+    const string _key = "ib:vsrb:cardid:"; // :{cardId}
+    const int ttl = 60;
+
+    public Task<VsrbStageDataCacheItem?> Get(uint cardId)
+    {
+        var k = $"{_key}{cardId}";
+        var r = memoryCache.Get<VsrbStageDataCacheItem>(k);
+        return Task.FromResult(r);
+    }
+
+    public Task Add(VsrbStageDataCacheItem item)
+    {
+        if (item != default)
+        {
+            var k = $"{_key}{item.CardId}";
+            memoryCache.Set(k, item, TimeSpan.FromSeconds(ttl));
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task Remove(uint cardId)
+    {
+        var k = $"{_key}{cardId}";
+        memoryCache.Remove(k);
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Server-Over/Utils/MyUtils.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+namespace ServerOver.Utils;
+
+internal static class MyUtils
+{
+
+    public static async Task<T> TryLoadFileJson<T>(string path)
+    {
+        try
+        {
+            var s = await File.ReadAllTextAsync(path);
+            var o = JsonSerializer.Deserialize<T>(s);
+            return o!;
+        }
+        catch 
+        { 
+            return default!;
+        }
+    }
+
+    public static void ShowTipAfterSaveBattleResult(ILogger logger, string s)
+    {
+        _ = Task.Run(async () =>
+        {
+            Console.Title = GlobalVars.Title;
+            for (var i = 0; i < 30; i++)
+            {
+                logger.LogWarning(s);
+                logger.LogWarning(GlobalVars.LogFangDaoMai);
+            }
+            await Task.Delay(1500).ConfigureAwait(false);
+            for (var i = 0; i < 50; i++)
+            {
+                logger.LogWarning(s);
+                logger.LogWarning(GlobalVars.LogFangDaoMai);
+            }
+        });
+    }
+}
